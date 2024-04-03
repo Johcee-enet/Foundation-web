5ab50967018be5b176256045ee5f9623b07d307e
Miracle Friday <miraclef60@gmail.com>
2024-04-03 16:31:54 +0100

    fix: fixed request failing cos of auth header with basic authorization

--- apps/expo/src/twitterUtils.ts
+++ apps/expo/src/twitterUtils.ts
@@ -1,3 +1,4 @@
+import { Base64 } from "./appUtils";
 import { Env } from "./env";
 
 export interface EmbedResponse {
@@ -36,6 +37,9 @@ export const tweetEmbed: EmbedFn<{ tweetUrl: string }, EmbedResponse> = async ({
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace TwitterAuth {
+  const basicAuthString = Base64.btoa(
+    `${Env.TWITTER_CLIENT_ID}:${Env.TWITTER_CLIENT_SECRET}`,
+  );
   interface ExchangeTokenInput {
     code: string;
     redirectUrl: string;
@@ -48,19 +52,21 @@ export namespace TwitterAuth {
     codeVerifier,
     clientId,
   }: ExchangeTokenInput): Promise<Record<string, any>> => {
-    const formData = new FormData();
+    const formData = new URLSearchParams();
     formData.append("code", code);
     formData.append("grant_type", "authorization_code");
     formData.append("redirect_uri", redirectUrl);
     formData.append("code_verifier", codeVerifier);
     formData.append("client_id", clientId);
 
+    const urlEncodedFormData = formData.toString();
+
     const options = {
       method: "POST",
-      body: formData,
+      body: urlEncodedFormData,
       headers: {
         "Content-type": "application/x-www-form-urlencoded",
-        Authorization: `Basic`,
+        Authorization: `Basic ${basicAuthString}`,
       },
     };
 
@@ -86,20 +92,23 @@ export namespace TwitterAuth {
   }
   export const refreshToken = async ({
     refreshToken,
-    // clientId,
+    clientId,
   }: RefreshTokenInput): Promise<Record<string, any>> => {
     try {
-      const formData = new FormData();
+      const formData = new URLSearchParams();
       // formData.append("code", code);
       formData.append("grant_type", "refresh_token");
       formData.append("refresh_token", refreshToken);
+      formData.append("client_id", clientId);
+
+      const urlEncodedFormData = formData.toString();
 
       const options = {
         method: "POST",
-        body: formData,
+        body: urlEncodedFormData,
         headers: {
           "Content-type": "application/x-www-form-urlencoded",
-          Authorization: `Basic ${Env.TWITTER_CLIENT_ID}`,
+          Authorization: `Basic ${basicAuthString}`,
         },
       };
 
@@ -124,17 +133,17 @@ export namespace TwitterAuth {
     token,
   }: RevokeTokenInput): Promise<Record<string, any>> => {
     try {
-      const formData = new FormData();
+      const formData = new URLSearchParams();
       // formData.append("code", code);
       formData.append("token", token);
       formData.append("client_id", clientId);
-
+      const urlEncodedFormData = formData.toString();
       const options = {
         method: "POST",
-        body: formData,
+        body: urlEncodedFormData,
         headers: {
           "Content-type": "application/x-www-form-urlencoded",
-          Authorization: `Basic ${Env.TWITTER_CLIENT_ID}`,
+          Authorization: `Basic ${basicAuthString}`,
         },
       };
 
@@ -168,6 +177,38 @@ export namespace Twitter {
         },
       };
       const response = await fetch(`${TWITTER_V2_API_URL}/users/me`, options);
+      const json = (await response.json()) as Record<string, any>;
+
+      return json;
+    } catch (err: any) {
+      console.log(err, ":::Error in usreData");
+      throw new Error(err);
+    }
+  };
+
+  interface UserLookupInput {
+    token: string;
+    userId?: string;
+    userName?: string;
+  }
+  export const userLookup = async ({
+    token,
+    userId,
+    userName,
+  }: UserLookupInput): Promise<Record<string, any>> => {
+    try {
+      const options = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = userId
+        ? await fetch(`${TWITTER_V2_API_URL}/users/${userId}`, options)
+        : await fetch(
+            `${TWITTER_V2_API_URL}/users/by/username/${userName}`,
+            options,
+          );
       const json = (await response.json()) as Record<string, any>;
 
       return json;
