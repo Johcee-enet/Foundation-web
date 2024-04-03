edea0a7273934d72c9139093701ece0cf4269abc
Miracle Friday <miraclef60@gmail.com>
2024-04-03 14:04:13 +0100

    feat: added twitter API utility handlers for auth flow and calls

--- apps/expo/src/twitterUtils.ts
+++ apps/expo/src/twitterUtils.ts
@@ -151,3 +151,148 @@ export namespace TwitterAuth {
     }
   };
 }
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Twitter {
+  interface UserDataInput {
+    token: string;
+  }
+  export const userData = async ({
+    token,
+  }: UserDataInput): Promise<Record<string, any>> => {
+    try {
+      const options = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await fetch(`${TWITTER_V2_API_URL}/users/me`, options);
+      const json = (await response.json()) as Record<string, any>;
+
+      return json;
+    } catch (err: any) {
+      console.log(err, ":::Error in usreData");
+      throw new Error(err);
+    }
+  };
+
+  export const follow = async ({
+    token,
+    profileId,
+  }: {
+    token: string;
+    profileId: string;
+  }) => {
+    try {
+      const options = {
+        headers: {
+          method: "POST",
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await fetch(
+        `${TWITTER_V2_API_URL}/users/${profileId}/following`,
+        options,
+      );
+      const json = (await response.json()) as Record<string, any>;
+
+      return json;
+    } catch (err: any) {
+      console.log(err, ":::Error in follow API");
+      throw new Error(err);
+    }
+  };
+
+  export const like = async ({
+    token,
+    userTwitterId,
+    tweetId,
+  }: {
+    token: string;
+    userTwitterId: string;
+    tweetId: string;
+  }) => {
+    try {
+      const options = {
+        headers: {
+          method: "POST",
+          body: JSON.stringify({
+            tweet_id: tweetId,
+          }),
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await fetch(
+        `${TWITTER_V2_API_URL}/users/${userTwitterId}/likes`,
+        options,
+      );
+      const json = (await response.json()) as Record<string, any>;
+
+      return json;
+    } catch (err: any) {
+      console.log(err, ":::Error in like API");
+      throw new Error(err);
+    }
+  };
+
+  export const repost = async ({
+    token,
+    tweetId,
+    userTwitterId,
+  }: {
+    token: string;
+    tweetId: string;
+    userTwitterId: string;
+  }) => {
+    try {
+      const options = {
+        headers: {
+          method: "POST",
+          body: JSON.stringify({
+            tweet_id: tweetId,
+          }),
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await fetch(
+        `${TWITTER_V2_API_URL}/users/${userTwitterId}/retweets`,
+        options,
+      );
+      const json = (await response.json()) as Record<string, any>;
+
+      return json;
+    } catch (err: any) {
+      console.log(err, ":::Error in repost API");
+      throw new Error(err);
+    }
+  };
+
+  // export const comment = async ({token, userTwitterId, tweetId}: {token: string; userTwitterId: string; tweetId: string}) => {
+  //   try {
+  //     const options = {
+  //       headers: {
+  //         method: "POST",
+  //         body: JSON.stringify({
+  //           tweet_id: tweetId,
+  //         }),
+  //         "Content-type": "application/json",
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     };
+  //     const response = await fetch(
+  //       `${TWITTER_V2_API_URL}/users/${userTwitterId}/retweets`,
+  //       options,
+  //     );
+  //     const json = (await response.json()) as Record<string, any>;
+
+  //     return json;
+  //   } catch (err: any) {
+  //     console.log(err, ":::Error in repost API");
+  //     throw new Error(err);
+  //   }
+  // };
+}
