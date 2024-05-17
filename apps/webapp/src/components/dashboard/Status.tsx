98acac93900d7eb1b0d86cfe995c1298e05cf8e5
Miracle Friday <miraclef60@gmail.com>
2024-05-17 18:31:33 +0100

    feat(components): added loader component and fixed shareLink, Status and TrackPositions components

--- apps/webapp/src/components/dashboard/Status.tsx
+++ apps/webapp/src/components/dashboard/Status.tsx
@@ -1,52 +1,48 @@
+"use client";
+
+import { FC } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import MiningStats from "./MiningStats";
 import SocialStats from "./SocialStats";
 
-const Status = () => {
-    return (
-        <>
-            <Tabs defaultValue="mining" className="drop-shadow-sm">
-                <TabsList className="grid w-full grid-cols-2 p-0">
-                    <TabsTrigger
-                        value="mining"
-                        className="tabs-trigger"
-                    >
-                        Mining
-                    </TabsTrigger>
-                    <TabsTrigger
-                        value="social"
-                        className="tabs-trigger"
-                    >
-                        Social Xps
-                    </TabsTrigger>
-                </TabsList>
-                <TabsContent
-                    value="mining"
-                    className="tab-content"
-                >
-                    {/* props for mining stats info */}
-                    <MiningStats mined={49500.71}
-                        mining={6}
-                        time={"19hrs  23m  12s"}
-                        rate={0.25}
-                    />
-                </TabsContent>
-                <TabsContent
-                    value="social"
-                    className="tab-content"
-                >
-                       {/* props for social stats info */}
-                    <SocialStats
-                        earned={35545700}
-                        claimed={35500000}
-                        referral={500000}
-              
-                        multiplier={10}
-                    />
-                </TabsContent>
-            </Tabs>
-        </>
-    )
-}
+const Status: FC<{
+  minedCount: number;
+  mineRate: number;
+  mineHours: number;
+}> = ({ minedCount, mineRate, mineHours }) => {
+  return (
+    <>
+      <Tabs defaultValue="mining" className="drop-shadow-sm">
+        <TabsList className="grid w-full grid-cols-2 p-0">
+          <TabsTrigger value="mining" className="tabs-trigger">
+            Mining
+          </TabsTrigger>
+          <TabsTrigger value="social" className="tabs-trigger">
+            Social Xps
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="mining" className="tab-content">
+          {/* props for mining stats info */}
+          <MiningStats
+            mined={Number(minedCount ?? 49500.71)}
+            mining={Number(mineHours ?? 6)}
+            time={"19hrs  23m  12s"}
+            rate={Number(mineRate ?? 0.25)}
+          />
+        </TabsContent>
+        <TabsContent value="social" className="tab-content">
+          {/* props for social stats info */}
+          <SocialStats
+            earned={35545700}
+            claimed={35500000}
+            referral={500000}
+            multiplier={10}
+          />
+        </TabsContent>
+      </Tabs>
+    </>
+  );
+};
 
-export default Status
+export default Status;
