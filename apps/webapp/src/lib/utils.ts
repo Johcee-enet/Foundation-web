359c896f43980ce8a08fed62d389851f0d1346a3
Miracle Friday <miraclef60@gmail.com>
2024-05-20 09:47:12 +0100

    fix: added delay function to utils file

--- apps/webapp/src/lib/utils.ts
+++ apps/webapp/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function delay(seconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
